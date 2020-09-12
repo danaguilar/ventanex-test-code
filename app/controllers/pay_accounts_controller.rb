8f870f4e9bae824868be17b55860fd959af3d363
Daniel <danaguilar6@gmail.com>
2020-09-12 11:45:49 -0400

    Added ability for users to create new pay account
    In order for users to have the ability to create new pay accounts, the
    following was added.
    
    - Updates to the `create` and `new` pay_acount_controller actions. The
    new controller action supplies an empty pay_account model for use in
    associated view's form. The create action takes a filtered version of
    the parameters returned by that form (known as "strong params"), uses
    them to create a new pay_account object, and saves that object to the
    database. It then redirects back to the index action so the user can
    view their new pay_account with all the others
    
    - Creation of a pay_account `new` view along with some minor css to size
    it properly.
    
    - Updates to the pay_account `index` view to link the create button to
    the new view.

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -6,7 +6,16 @@ class PayAccountsController < ApplicationController
     @pay_accounts = PayAccount.all
   end
 
-  def create
+  def new
+    @pay_account = PayAccount.new
   end
 
+  def create
+    PayAccount.create(pay_account_params)
+    redirect_to pay_accounts_url
+  end
+
+  def pay_account_params
+    params.require(:pay_account).permit(:nickname, :account_number, :account_type)
+  end
 end
