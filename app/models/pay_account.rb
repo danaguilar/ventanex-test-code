e4d13bb8ff9d8a812a7c2a0da91d1b952fc50ba2
Daniel <danaguilar6@gmail.com>
2020-09-13 23:41:32 -0400

    Added navbar
    There's not a lot to show for a navbar so I added a dropdown that shows
    the last five accounts added. Creates a scope for that as well on the
    PayAccount model

--- app/models/pay_account.rb
+++ app/models/pay_account.rb
@@ -3,5 +3,6 @@ class PayAccount < ActiveRecord::Base
   validates :account_number,
     length: { is: 12, wrong_length: "must contain exactly 12 characters" }
 
+  scope :recent_accounts, -> { order(created_at: :desc).limit(5) }
   enum account_type: {bank_account:  1, debit_card: 2}
 end
